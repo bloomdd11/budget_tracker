2055d5563c91278d7bcd3d356d72d62c12acee93
kettlothsuhoo <kh685132@gmail.com>
2022-11-30 18:14:48 +0630

    delete and update routes

--- routes/budgetRoute.js
+++ routes/budgetRoute.js
@@ -4,10 +4,13 @@ const router = express.Router()
 const Budget = require('../model/Budget')
 const { body, param, validationResult } = require('express-validator')
 
+// READ ROUTE
 router.get('/', async (req, res) => {
   let budget = await Budget.find({})
   return res.json(budget)
 })
+
+// CREATE ROUTE
 router.post('/', [
   body('amount').not().isEmpty(),
   body('detail').not().isEmpty()
@@ -29,6 +32,51 @@ router.post('/', [
     return res.json({ msg: budget })
   } catch (error) {
     return res.status(404).json({ error })
+  }
+})
+
+// UPDATE ROUTE
+router.put('/:id', [
+  param('id').isMongoId()
+], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    let budget = await Budget.findById({ _id: req.params.id })
+    if (budget) {
+      budget = await Budget.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+      await budget.save()
+      return res.json({ msg: budget })
+    } else {
+      return res.status(404).json({ msg: 'no document with that id' })
+    }
+  } catch (error) {
+    return res.status(404).json({ return: true, error })
+  }
+})
+
+// DELETE ROUTE
+router.delete('/:id', [
+  param('id').isMongoId()
+], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ return: true, errors: errors.array() })
+  }
+
+  try {
+    let budget = await Budget.findById({ _id: req.params.id })
+    if (budget) {
+      budget = await Budget.findByIdAndDelete({ _id: req.params.id })
+      return res.status(200).json({ msg: 'deleted' })
+    } else {
+      return res.status(404).json({ msg: 'no document with that id' })
+    }
+  } catch (error) {
+    return res.status(404).json({ return: true, error })
   }
 })
 
--- test.js
+++ test.js
@@ -1,30 +1,30 @@
-let id = [...Array(16)].map((e) => (Math.random() * 16 | 0).toString(16)).join('')
+// let id = [...Array(16)].map((e) => (Math.random() * 16 | 0).toString(16)).join('')
 
-// return MMT from node time
-const getMMT = () => {
-  // Get UTC time
-  const time = (new Date().toUTCString().split(' ')[4].split(':'));
+// // return MMT from node time
+// const getMMT = () => {
+//   // Get UTC time
+//   const time = (new Date().toUTCString().split(' ')[4].split(':'));
 
-  const timeChange = (o, n) => (+o) + n
+//   const timeChange = (o, n) => (+o) + n
 
-  // after adding GMT +6:30 but not correct
-  let hr = timeChange(time[0], 6)
-  let min = timeChange(time[1], 30)
-  let sec = +time[2]
+//   // after adding GMT +6:30 but not correct
+//   let hr = timeChange(time[0], 6)
+//   let min = timeChange(time[1], 30)
+//   let sec = +time[2]
 
-  // correct min and hour
-  if (min > 60) {
-    hr++
-    min = min - 60
-  }
-  sec < 10 ? sec = `0${sec.toString()}` : sec.toString()
-  let newMMT = [hr.toString(), min.toString(), sec].join(':')
+//   // correct min and hour
+//   if (min > 60) {
+//     hr++
+//     min = min - 60
+//   }
+//   sec < 10 ? sec = `0${sec.toString()}` : sec.toString()
+//   let newMMT = [hr.toString(), min.toString(), sec].join(':')
 
-  let newUTC = new Date().toUTCString().split(' ')
-  newUTC[4] = newMMT
+//   let newUTC = new Date().toUTCString().split(' ')
+//   newUTC[4] = newMMT
 
-  console.log(newUTC.join(' '));
-  return newUTC.join(' ')
-}
+//   console.log(newUTC.join(' '));
+//   return newUTC.join(' ')
+// }
 
-getMMT()
+// getMMT()
